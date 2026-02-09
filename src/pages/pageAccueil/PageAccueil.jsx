529603f119b3ae111ecfc668f24b8931601fe42e
Julien POIRIER <poirierjulien@live.fr>
2026-02-09 11:15:55 +0100

    form

--- src/pages/pageAccueil/PageAccueil.jsx
+++ src/pages/pageAccueil/PageAccueil.jsx
@@ -8,6 +8,19 @@ const PageAccueil = () => {
     <>
       <p>Page Accueil</p>
       <Header />
+      <form action="" style={{ margin: "20px" }}>
+        <div>
+          <label htmlFor="email">Merci de saisir de votre email</label>
+          <input type="text" name="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            Merci de saisir de votre mot de passe
+          </label>
+          <input type="password" name="password" id="password" />
+        </div>
+        <button>Se connecter</button>
+      </form>
     </>
   );
 };
