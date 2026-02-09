45774a984d0930c6e6b3069dd16a71cf4de7dc4a
Julien POIRIER <poirierjulien@live.fr>
2026-02-09 09:51:16 +0100

    routing init

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import App from "./App.jsx";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <p>404</p>,
+  },
+  {
+    path: "/tasks",
+    element: <p>Tasks</p>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
