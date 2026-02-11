48c1db58acf79cffe9b48401a6284da8e80f7ed4
Julien POIRIER <poirierjulien@live.fr>
2026-02-11 12:16:51 +0100

    adapt tabs

--- example_expo_quizz/app/_layout.tsx
+++ example_expo_quizz/app/_layout.tsx
@@ -1,10 +1,27 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Tabs } from "expo-router";
-
 const RootLayout = () => {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ headerShown: false }} />
-      <Tabs.Screen name="quizz" options={{ href: "/quizz" }} />
+    <Tabs screenOptions={{ tabBarActiveTintColor: "black" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="quizz"
+        options={{
+          href: "/quizz",
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="pencil" color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
